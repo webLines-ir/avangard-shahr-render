10f79c252a54a2a45b1d87abaa78442a6975cd5c
Mjjafari66 <132228351+Mjjafari66@users.noreply.github.com>
2025-08-04 17:12:19 +0330

    mj

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' &&
     componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  base: process.env.NODE_ENV === 'production' ? "/avangard-shahr-render/" : "/",
+});
